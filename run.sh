1332e1086444dbe7aa6d0bfcc07dc97ed58da8e2
ReanGD <ReanGD@yandex.ru>
2015-04-16 16:41:54 +0300

    init commit

---
+++ run.sh
@@ -0,0 +1,9 @@
+[ ! -f /www/app/uwsgi_params ] && cp /home/uwsgi_params /www/app/
+[ ! -f /www/app/uwsgi.ini ] && cp /home/uwsgi.ini /www/app/
+[ ! -f /www/app/nginx-app.conf ] && cp /home/nginx-app.conf /www/app/
+
+ln -s /www/app/nginx-app.conf /etc/nginx/sites-enabled/
+mkdir -p /www/tmp/spooler
+
+uwsgi --ini /www/app/uwsgi.ini
+nginx
