76fbeac1fed4e1e7e0fcc172aa044deb4b798746
ReanGD <reangd@yandex.ru>
2016-01-05 21:26:50 +0300

    реорганизовал структуру файлов

---
+++ work-fitnesse/clean.sh
@@ -0,0 +1,14 @@
+rm -rf /var/cache/apt/*
+rm -rf /var/lib/apt/*
+rm -rf /var/lib/dpkg/*
+rm -rf /var/lib/cache/*
+rm -rf /var/lib/log/*
+rm -rf /var/tmp/*
+rm -rf /tmp/*
+rm -rf /usr/share/i18n/
+rm -rf /usr/share/doc/
+rm -rf /usr/share/locale/
+rm -rf /usr/share/man/
+rm -rf /usr/sbin/locale-gen
+rm -rf /usr/sbin/update-locale
+rm -rf /usr/sbin/validlocale
---
+++ work-fitnesse/install_static.sh
@@ -0,0 +1,15 @@
+mkdir -p /www/app /www/static/jquery /www/static/bootstrap /www/static/django_ajax /www/static/highcharts
+
+cp /usr/local/lib/python3.4/dist-packages/django_ajax/static/django_ajax/js/*.min.* /www/static/django_ajax
+
+unzip /home/bootstrap.zip -d /home/bootstrap
+cp -r /home/bootstrap/bootstrap-3.3.6-dist/* /www/static/bootstrap/
+rm -rf /home/bootstrap
+
+unzip /home/jquery.zip  -d /home/jquery
+cp /home/jquery/jquery-2.1.4/dist/*.min.* /www/static/jquery/
+rm -rf /home/jquery
+
+git clone --branch v4.0.0 https://github.com/highslide-software/highcharts.com.git /www/static/highcharts
+cd /www/static/highcharts
+rm -rf samples tools utils test studies .git bower.json build.md build.properties build.xml changelog.htm invalidations.txt readme.md ant
