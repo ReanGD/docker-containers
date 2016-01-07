7ceda04201c65ae184a8a47e22547cc1448190f4
ReanGD <reangd@yandex.ru>
2016-01-07 22:29:21 +0300

    full supervisor config

--- work-fitnesse/run.sh
+++ work-fitnesse/run.sh
@@ -2,6 +2,7 @@
 [ ! -f /www/app/uwsgi.ini ] && cp /home/uwsgi.ini /www/app/
 [ ! -f /www/app/nginx-app.conf ] && cp /home/nginx-app.conf /www/app/
 [ ! -f /www/app/supervisord.conf ] && cp /home/supervisord.conf /www/app/
+[ ! -f /www/app/redis.conf ] && cp /home/redis.conf /www/app/
 
 ln -s /www/app/nginx-app.conf /etc/nginx/sites-enabled/
 
