d364822aa7d22365ec3c42175bdfad6142e855af
Vladimir <ReanGD@yandex.ru>
2016-04-28 12:40:36 +0300

    Полный путь к known_hosts

--- work-web-builder/run.sh
+++ work-web-builder/run.sh
@@ -1,4 +1,4 @@
 chmod 777 /jenkins
 cp /jenkins/id_rsa /home/jenkins/.ssh/
-ssh-keyscan -H 172.28.1.31 >> ~/.ssh/known_hosts
+ssh-keyscan -H 172.28.1.31 >> /home/jenkins/.ssh/known_hosts
 /usr/sbin/sshd -D
